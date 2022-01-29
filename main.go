d635e7e1d9bcda8103a98c6051ccde12a29cc04a
luiz-otavio <luizfarias2006@hotmail.com>
2022-01-29 16:29:03 -0300

    chore: add delete and fix some routes

--- main.go
+++ main.go
@@ -18,7 +18,11 @@ func main() {
 	connector := galax.NewConnector(DSN_MARIADB)
 	client := galax.NewRedis(DSN_REDIS)
 
-	connector.Database.AutoMigrate(&galax.Account{})
+	err := connector.Database.AutoMigrate(&galax.Account{}, &galax.GroupInfo{}, &galax.MetadataSet{})
+
+	if err != nil {
+		panic(err)
+	}
 
 	app := fiber.New(fiber.Config{
 		CaseSensitive:     true,
@@ -29,16 +33,19 @@ func main() {
 	cache := galax.NewCache(client)
 	router := galax.NewRouter(connector.Database, cache)
 
+	galax.Initialize(connector.Database)
+
 	app.Put("/account/create", router.CreateAccount)
 	app.Get("/account/search", router.SearchAccount)
+	app.Delete("/account/delete", router.DeleteAccount)
 	app.Patch("/account/metadata/update", router.UpdateMetadata)
-	app.Delete("/account/group/remove", router.DeleteAccount)
+	app.Delete("/account/group/remove", router.DeleteGroup)
 	app.Post("/account/group/insert", router.InsertGroup)
 	app.Patch("/account/name", router.UpdateName)
 	app.Patch("/account/cash/update", router.UpdateCash)
 	app.Patch("/account/cash/sum", router.SumCash)
 
-	app.Listen(":8080")
+	app.Listen(":5896")
 }
 
 func env(key string) string {
